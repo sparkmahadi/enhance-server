0e5296f2dc0ee39954e257b2cd1f7d60b66afb59
sparkmahadi <mahadi.developer@gmail.comm>
2022-11-08 20:51:33 +0600

    more api added

--- index.js
+++ index.js
@@ -13,13 +13,26 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gp7ekja.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
+async function run() {
     const serviceCollection = client.db("enhance").collection("services");
 
-    app.get('/services', async(req, res)=>{
+    app.get('/', async (req, res) => {
+        const query = {};
+        const cursor = serviceCollection.find(query);
+        const result = await cursor.limit(3).toArray();
+        res.send(result);
+    })
+
+    app.get('/services', async (req, res) => {
         const query = {};
         const cursor = serviceCollection.find(query);
         const result = await cursor.toArray();
+        res.send(result);
+    })
+    app.get('/services/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await serviceCollection.findOne(query);
         res.send(result);
     })
 }
@@ -27,9 +40,9 @@ async function run(){
 run().catch(err => console.log(err))
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('enhance server is running')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
 })
