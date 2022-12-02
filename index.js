fd05929d9f91a0d9c7a17adf7968caf6fe5bfa6d
sparkmahadi <mahadi.development@gmail.com>
2022-12-02 19:36:57 +0600

    adding more apis

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -16,7 +18,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 app.post('/jwt', (req, res) => {
     const user = req.body;
-    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '24h' })
     res.send({ token });
 })
 
@@ -40,7 +42,10 @@ function verifyJWT(req, res, next) {
 async function run() {
     const serviceCollection = client.db("enhance").collection("services");
     const reviewCollection = client.db("enhance").collection("reviews");
+    const usersCollection = client.db("enhance").collection("users");
+    const serviceBookingsCollection = client.db("enhance").collection("serviceBookings");
 
+    // to load three services at landing page
     app.get('/', async (req, res) => {
         const query = {};
         const cursor = serviceCollection.find(query);
@@ -48,12 +53,31 @@ async function run() {
         res.send(result);
     })
 
+    app.post('/create-payment-intent', async (req, res) => {
+        const booked = req.body;
+        const price = booked.price;
+        const amount = price * 100;
+
+        const paymentIntent = await stripe.paymentIntents.create({
+            currency: 'usd',
+            amount: amount,
+            'payment_method_types': [
+                'card'
+            ],
+        });
+
+        res.send({ clientSecret: paymentIntent.client_secret })
+    })
+
+    // to get all the services
     app.get('/services', async (req, res) => {
         const query = {};
         const cursor = serviceCollection.find(query);
         const result = await cursor.toArray();
         res.send(result);
     })
+
+    // to load a specific service
     app.get('/services/:id', async (req, res) => {
         const id = req.params.id;
         const query = { _id: ObjectId(id) };
@@ -61,6 +85,7 @@ async function run() {
         res.send(result);
     })
 
+    // to post new services
     app.post('/services', async (req, res) => {
         const service = req.body;
         const result = await serviceCollection.insertOne(service);
@@ -75,8 +100,49 @@ async function run() {
         res.send(result);
     })
 
+    // to find service bookings by user
+    app.get('/serviceBookings', async (req, res) => {
+        const email = req.query.email;
+        const query = { buyerEmail: email }
+        const result = await serviceBookingsCollection.find(query).toArray();
+        res.send(result);
+    })
+
+    // to find a service booking for payment
+    app.get('/serviceBookings/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) }
+        const result = await serviceBookingsCollection.findOne(query);
+        res.send(result);
+    })
+
+    // to book a service
+    app.post('/serviceBookings', async (req, res) => {
+        const email = req.query.email;
+        const serviceId = req.query.serviceId;
+        const booking = req.body;
+        const result = await serviceBookingsCollection.insertOne(booking);
+        res.send(result);
+    })
+
+    // to update a service booking after payment
+    app.put('/serviceBookings/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const paymentInfo = req.body;
+        const option = { upsert: true };
+        const updatedBooking = {
+            $set: {
+                payment: paymentInfo.payment,
+                paymentTime: paymentInfo.paymentTime
+            }
+        }
+        const result = await serviceBookingsCollection.updateOne(query, updatedBooking, option);
+        res.send(result);
+    })
+
     // load my reviews
-    app.get('/MyReviews', verifyJWT, async (req, res) => {
+    app.get('/myreviews', verifyJWT, async (req, res) => {
         const decoded = req.decoded;
         if (decoded.email !== req.query.email) {
             res.status(403).send({ message: 'unauthorized access' })
@@ -89,6 +155,14 @@ async function run() {
             }
         }
         const cursor = reviewCollection.find(query);
+        const reviews = await cursor.toArray();
+        res.send(reviews);
+    })
+
+    // load all reviews by users
+    app.get('/reviews', async (req, res) => {
+        const query = {};
+        const cursor = reviewCollection.find(query).sort({ createdAt: -1 });
         const reviews = await cursor.toArray();
         res.send(reviews);
     })
@@ -126,10 +200,32 @@ async function run() {
         res.send(result);
     })
 
+    // to delete a review
     app.delete('/reviews/:id', async (req, res) => {
         const id = req.params.id;
         const query = { _id: ObjectId(id) };
         const result = await reviewCollection.deleteOne(query);
+        res.send(result);
+    })
+
+    // to get a single user to verify admin
+    app.get('/users/:email', async (req, res) => {
+        const email = req.params.email;
+        const query = { email: email };
+        const user = await usersCollection.findOne(query);
+        res.send(user);
+    })
+    // to get all user
+    app.get('/users', async (req, res) => {
+        const query = {};
+        const users = await usersCollection.find(query).toArray();
+        res.send(users);
+    })
+
+    // to post a user
+    app.post('/users', async (req, res) => {
+        const user = req.body;
+        const result = await usersCollection.insertOne(user);
         res.send(result);
     })
 }
